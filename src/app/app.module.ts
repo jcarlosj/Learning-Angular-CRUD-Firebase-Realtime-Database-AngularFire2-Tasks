df9f69ad09682df1b97455f48e781e78562b5fab
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-06-25 11:18:47 -0500

    Structure: Crea 'Tasks, AddTask Components', 'Task Service', 'Interface Task'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule
